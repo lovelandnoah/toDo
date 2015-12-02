ef530a478e1f75d1b9c2006e9e2ccd4bc200b632
Noah Loveland <lovelandnoah@gmail.com>
2015-12-02 16:13:59 -0700

    more stuff

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -21,6 +21,26 @@ class PostsController < ApplicationController
   	end
 	end
 
+	def edit
+		@post = Post.find(params[:id])
+	end
+
+	def update
+		@post = Post.find(params[:id])
+
+		if @post.update(post_params)
+			redirect_to posts_path
+		else
+			render :edit
+		end		
+	end
+	
+	def destroy
+
+		
+	end
+
+
 	private
 
 	def post_params
